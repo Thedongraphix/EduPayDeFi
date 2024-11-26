8786fe5851ec31180edb098e94d8c9c27e7f4110
Thedongraphix <chrisoketchcn@gmail.com>
2024-11-26 12:32:03 +0300

    New landing

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -3,7 +3,7 @@
 import { motion } from "framer-motion";
 import { Button } from "@/components/ui/button";
 import { AnimatedGradientBorder } from "@/components/ui/animated-gradient-border";
-import { ArrowRight, Wallet } from "lucide-react";
+import { ArrowRight} from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
