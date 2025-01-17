e6585817dc493e47e6dc97f043efd5e0e079d83c
Chris Oketch <chrisoketchcn@gmail.com>
2025-01-17 11:32:39 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
- 
-};
-
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['images.unsplash.com'],
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'images.unsplash.com',
+          port: '',
+          pathname: '/**',
+        },
+      ],
+    },
+  }
+  
+  module.exports = nextConfig
