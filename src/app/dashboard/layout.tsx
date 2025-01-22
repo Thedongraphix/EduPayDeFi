f27d3bb8bef71dcdc136bc6a14a4382e14ec057d
Thedongraphix <chrisoketchcn@gmail.com>
2025-01-22 23:19:17 +0300

    New changes

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -36,7 +36,7 @@ export default function DashboardLayout({
         </SheetContent>
       </Sheet>
 
-      {/* Main content */}
+      {/* Main content */} 
       <div className="flex-1 flex flex-col md:pl-64">
         <Header />
         <main className="flex-1 overflow-y-auto p-6">
