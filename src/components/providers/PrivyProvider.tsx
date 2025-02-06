03608b2d985f82573732b7910e8ca54665317ac2
Thedongraphix <chrisoketchcn@gmail.com>
2025-02-06 18:16:21 +0300

    Trial

--- src/components/providers/PrivyProvider.tsx
+++ src/components/providers/PrivyProvider.tsx
@@ -14,7 +14,7 @@ export default function PrivyProviderWrapper({ children }: PrivyProviderWrapperP
     <PrivyProvider
       appId='cm35xkh1r03bvcpeg8p67h8mn'
       config={{
-        loginMethods: ['email', 'wallet','farcaster'] ,
+        loginMethods: ['email', 'wallet','farcaster','google','github'], 
         appearance: {
           theme: 'dark',
           accentColor: '#676FFF',
